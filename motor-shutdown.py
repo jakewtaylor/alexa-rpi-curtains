57979477fde696ac39b63fd0192f14302627c2c7
Jake Taylor <me@jaketaylor.co>
2021-03-19 15:48:17 +0000

    convert args to int

--- motor-shutdown.py
+++ motor-shutdown.py
@@ -4,8 +4,8 @@ import RPi.GPIO as GPIO
 import time
 import sys
 
-wait_time = sys.argv[1]
-stby = sys.argv[2]
+wait_time = int(sys.argv[1])
+stby = int(sys.argv[2])
 
 print('wait_time and stby: ', wait_time, stby)
 
